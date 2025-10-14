5fdffa97fa6ce8e72e8752733f1cf9d690ef4fef
diegozc792-bit <dcervantesapa@unsa.edu.pe>
2025-10-14 17:29:51 -0500

    set para dni y arreglo de tipo de variable de objeto

--- Usuario.java
+++ Usuario.java
@@ -1,6 +1,6 @@
 public class Usuario {
     private String nombre;
-    private String dni;
+    private int dni;  //Mejora de variable
     public Usuario(String nombre, String dni){
         this.nombre=nombre;
         this.dni=dni;
@@ -17,7 +17,9 @@ public class Usuario {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-    		
+    	
+    public void int setDni(int dni) {
+    		this.dni=dni;
     }
     @Override
     public String toString() {
