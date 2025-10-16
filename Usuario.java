7ec956b31e8503a5dcca3c81d7880d670a092361
diegozc792-bit <dcervantesapa@unsa.edu.pe>
2025-10-16 06:03:15 -0500

    arreglando verificacion adeacuada

--- Usuario.java
+++ Usuario.java
@@ -2,32 +2,21 @@ public class Usuario {
     private String nombre;
     private int dni;  //Mejora de variable
     
-    //Constructor
-    public Usuario(String nombre, String dni){
-        this.nombre=nombre;
-        this.dni=dni;
-    
-        
-    //metodos setters y getters para atributos setters y getters
-    }
-    public String getNombre(){
-        return nombre;
-    }
-    public String getDni(){
-        return dni;
+    public Usuario(String nombre, int dni) {
+
+		this.nombre=nombre;
+		this.dni=dni;
+		
     }
     
-    //Creacion de setters
     public void setNombre(String nombre) {
-        this.nombre = nombre;
+        this.nombre=nombre;
+       
     }
-    	
-    //save
-    public void int setDni(int dni) {
-    		this.dni=dni;
+    
+    public void setDni(int dni) {
+        this.dni=dni;
+       
     }
-    @Override
-    public String toString() {
-        return "Usuario: " + nombre + " - DNI: " + dni;
-    }
-}
+}   
+  
